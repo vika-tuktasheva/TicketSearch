18b26d28ba882949ddf0111273d04f7d846f1407
vika-tuktasheva <konovalova_v@list.ru>
2022-06-01 00:11:45 +0400

    Add CI

--- src/main/java/ru/netology/TicketManager.java
+++ src/main/java/ru/netology/TicketManager.java
@@ -3,7 +3,7 @@ package ru.netology;
 import java.util.Arrays;
 
 public class TicketManager {
-    TicketRepository repository=new TicketRepository();
+    TicketRepository repository = new TicketRepository();
 
     //конструкторы
     public TicketManager() {
@@ -31,28 +31,28 @@ public class TicketManager {
     }
 
     //поиск аэропортам с сортировкой по цене
-    public Ticket[] findAll(String airportFrom, String airportTo){
-        Ticket[] results=new Ticket[0];
-        for (Ticket ticket:repository.findAll()             ) {
-            if (matches(ticket,airportFrom,airportTo)){
-                Ticket[] tmp=new Ticket[results.length+1];
-                for (int i=0;i< results.length;i++){
-                    tmp[i]=results[i];
+    public Ticket[] findAll(String airportFrom, String airportTo) {
+        Ticket[] results = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matches(ticket, airportFrom, airportTo)) {
+                Ticket[] tmp = new Ticket[results.length + 1];
+                for (int i = 0; i < results.length; i++) {
+                    tmp[i] = results[i];
                 }
-                tmp[tmp.length-1]=ticket;
-                results=tmp;
+                tmp[tmp.length - 1] = ticket;
+                results = tmp;
             }
         }
         Arrays.sort(results);
-        return  results;
+        return results;
     }
 
-//метод сравнения
+    //метод сравнения
     private boolean matches(Ticket ticket, String airportFrom, String airportTo) {
-        if (ticket.getAirportFrom()!=airportFrom){
-            return  false;
+        if (ticket.getAirportFrom() != airportFrom) {
+            return false;
         }
-        if(ticket.getAirportTo()!=airportTo){
+        if (ticket.getAirportTo() != airportTo) {
             return false;
         }
         return true;
--- src/test/java/ru/netology/TicketManagerTest.java
+++ src/test/java/ru/netology/TicketManagerTest.java
@@ -6,19 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
     @Test
-    void findAll(){
-        TicketManager manager=new TicketManager();
-        Ticket ticket1=new Ticket(1, 10000,"KUF", "LED", 120);
-        Ticket ticket2=new Ticket(2, 5000,"KUF", "MOW", 60);
-        Ticket ticket3=new Ticket(3, 15000,"DME", "GOJ", 90);
-        Ticket ticket4=new Ticket(4, 12000,"DME", "GOJ", 100);
+    void findAll() {
+        TicketManager manager = new TicketManager();
+        Ticket ticket1 = new Ticket(1, 10000, "KUF", "LED", 120);
+        Ticket ticket2 = new Ticket(2, 5000, "KUF", "MOW", 60);
+        Ticket ticket3 = new Ticket(3, 15000, "DME", "GOJ", 90);
+        Ticket ticket4 = new Ticket(4, 12000, "DME", "GOJ", 100);
 
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
 
-        Ticket[] expected=new Ticket[]{ticket4, ticket3};
+        Ticket[] expected = new Ticket[]{ticket4, ticket3};
         assertArrayEquals(expected, manager.findAll("DME", "GOJ"));
 
     }
